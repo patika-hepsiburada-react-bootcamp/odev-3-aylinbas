54cf1f5a434d8937ad3e0ace73b807523514dba4
aylinbas <ayln.bas.50@gmail.com>
2021-10-08 22:40:34 +0300

    chart

--- client/src/components/Charts.js
+++ client/src/components/Charts.js
@@ -3,22 +3,49 @@ import { Bar } from "react-chartjs-2"
 import { useLanguage } from '../context/LanguageContext';
 
 function Charts() {
+    var labels = [];
+    var data = [];
     const { languages } = useLanguage();
     {
         languages.map((item, i) => (
-            console.log("item" + item.name)
+            console.log("item" + item.name),
+            labels.push(item.name),
+            data.push(item.point)
 
         ))
     }
+    debugger;
+    var state = {
+        labels: labels,
+        datasets: [
+            {
+                label: 'Point',
+                backgroundColor: '#0B4C64',
+                borderColor: '#062632',
+                borderWidth: 3,
+                data: data
+            }
+        ]
+    }
+
     return (
         <div>
-            <Bar>
-                data={{
-                    labels: {}
-                }};
-            </Bar>
+            <Bar
+                data={state}
+                options={{
+                    title: {
+                        display: true,
+                        text: 'Voting rate',
+                        fontSize: 20
+                    },
+                    legend: {
+                        display: true,
+                        position: 'right'
+                    }
+                }}
+            />
         </div>
-    )
+    );
 }
 
 export default Charts
--- client/src/context/LanguageContext.js
+++ client/src/context/LanguageContext.js
@@ -4,7 +4,7 @@ const LanguageContext = createContext();
 
 export const LanguageProvider = ({ children }) => {
   const [languages, setLanguage] = useState([
-    { id: 0, name: "Swift", point: "0", }, { id: 1, name: "TypeScript", point: "0", }, { id: 2, name: "Go", point: "0", }, { id: 3, name: "C#", point: "0", }, { id: 4, name: "JavaScript", point: "0", }, { id: 5, name: "Java", point: "0", }, { id: 6, name: "Python", point: "0", }, { id: 7, name: "C", point: "0", }, { id: 8, name: "C++", point: "0", }
+    { id: 0, name: "Swift", point: "2", }, { id: 1, name: "TypeScript", point: "5", }, { id: 2, name: "Go", point: "4", }, { id: 3, name: "C#", point: "5", }, { id: 4, name: "JavaScript", point: "0", }, { id: 5, name: "Java", point: "0", }, { id: 6, name: "Python", point: "0", }, { id: 7, name: "C", point: "0", }, { id: 8, name: "C++", point: "0", }
 
   ]);
 
